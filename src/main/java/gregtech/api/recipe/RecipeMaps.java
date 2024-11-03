bd3cd954b3b481ca1cf59e1fd44b31ded5a1af1e
Allison-Rodrigo <95299389+Nockyx@users.noreply.github.com>
2024-11-03 15:35:10 -0300

    Added Precursor to the Tiering Mechanism

--- src/main/java/gregtech/api/recipe/RecipeMaps.java
+++ src/main/java/gregtech/api/recipe/RecipeMaps.java
@@ -25,6 +25,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Optional;
 
+import gregtech.api.recipe.metadata.SpatialAnomalyTierKey;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -605,6 +606,10 @@ public final class RecipeMaps {
         .progressBar(GTUITextures.PROGRESSBAR_ARROW_MULTIPLE)
         .logoPos(80, 50)
         .disableOptimize()
+        .neiRecipeComparator(
+            Comparator
+                .<GTRecipe, Integer>comparing(recipe -> recipe.getMetadataOrDefault(SpatialAnomalyTierKey.INSTANCE, 0))
+                .thenComparing(GTRecipe::compareTo))
         .build();
     public static final RecipeMap<RecipeMapBackend> spaceProjectFakeRecipes = RecipeMapBuilder
         .of("gt.recipe.fakespaceprojects")
